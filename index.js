be2dc21a30177aced97f1483688f911e2ff1f99b
rajurj92 <rajurj92@gmail.com>
2021-07-10 15:14:10 +0530

    nodepackage manager

---
+++ index.js
@@ -0,0 +1,8 @@
+console.log('npm First package')
+
+var chalk = require('chalk');
+
+console.log(chalk.green("Successfull!! ") + chalk.red.underline.bold("Error!! ") + chalk.blue("Warning!!"));
+
+
+
